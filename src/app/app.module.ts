5dba63ece3f592628948cf5ed9aed78b5c9e5273
dario.zubaray <dzubaray@imagosur.com>
2019-11-07 10:26:44 -0300

    combinando 2 reducers en el AppState

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { todoReducer } from './todo/todo.reducer';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -14,6 +13,7 @@ import { TodosItemComponent } from './todo/todos-item/todos-item.component';
 import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
 import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 import { environment } from '../environments/environment';
+import { appReducers } from './app.reducers';
 
 @NgModule({
   declarations: [
@@ -28,7 +28,7 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    StoreModule.forRoot({ todos: todoReducer}),
+    StoreModule.forRoot(appReducers),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production
--- src/app/app.reducers.ts
+++ src/app/app.reducers.ts
@@ -1,5 +1,20 @@
 import { Todo } from './todo/model/todo.model';
+import { ActionReducerMap } from '@ngrx/store';
+
+// Se acostumbra a hacerlo asi porque puede haber varios reducer con el mismo nombre
+import * as fromTodo from './todo/todo.reducer';
+import * as fromFiltro from './filter/filter.reducer';
+import * as fromFiltroActions from './filter/filter.actions';
+
+//import { todoReducer } from './todo/todo.reducer';
+//import { filtroReducer } from './filter/filter.reducer';
 
 export interface AppState {
     todos: Todo[];
+    filtro: fromFiltroActions.filtrosValidos;
+}
+
+export const appReducers: ActionReducerMap<AppState> = {
+  todos: fromTodo.todoReducer,
+  filtro: fromFiltro.filtroReducer
 }
