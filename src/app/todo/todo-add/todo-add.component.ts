ab9b672955c3075ec04eb2c60a8be7bb2d6aab5c
dario zubaray <dzubaray@gmail.com>
2019-11-06 21:54:03 -0300

    Agregando un TODO

--- src/app/todo/todo-add/todo-add.component.ts
+++ src/app/todo/todo-add/todo-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
+import { AgregarTodoAction } from '../todo.actions';
 
 @Component({
   selector: 'app-todo-add',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoAddComponent implements OnInit {
 
-  constructor() { }
+  txtInput: FormControl;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.txtInput = new FormControl('', Validators.required);
   }
 
+  agregarTodos() {
+    if (this.txtInput.invalid) {
+      return;
+    }
+    console.log(this.txtInput.value);
+
+    const accion = new AgregarTodoAction(this.txtInput.value);
+    this.store.dispatch(accion);
+
+    this.txtInput.setValue('');
+  }
 }
